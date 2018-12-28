9f72c057ea31dfc3c7593f927f7304db1d9f3423
serasdeva <serasdeva@gmail.com>
2018-12-28 23:10:59 +0700

    All edit

--- blog/models.py
+++ blog/models.py
@@ -1,16 +1,32 @@
 from django.db import models
 from django.shortcuts import reverse
+from django.utils.text import slugify
+from time import time
+
+
+def gen_slug(s):
+    new_slug = slugify(s)
+    return new_slug + '-' + str(int(time()))
+
+
 
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length = 200, db_index = True)
-    slug = models.SlugField(max_length=150, unique = True)
+    slug = models.SlugField(max_length=150, blank=True, unique = True)
     body = models.TextField(blank=True, db_index=True)
     date = models.DateField(auto_now_add=True)
     tag = models.ManyToManyField('Tag', blank=True, related_name='posts')
 
     def get_absolute_url(self):
         return reverse('post_datayl_url', kwargs={'slug':self.slug})
+
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = gen_slug(self.title)
+        super().save(*args, **kwargs)
+
 
     def __str__(self):
         return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,10 @@
-<<<<<<< HEAD
 from django.shortcuts import redirect, render, get_object_or_404
 
 from django.views.generic import View
 
 from .models import Post, Tag
-from .utils import ObjectDetailMixin
-from .forms import TagForm
+from .utils import ObjectDetailMixin, ObjectCreateMixin
+from .forms import TagForm, PostForm
 
 # Create your views here.
 def posts_list(request):
@@ -28,39 +27,11 @@ class TagDetail(ObjectDetailMixin, View):
 	template = 'blog/tag_datayl.html'
 
 
-class TagCreate(View):
-	def get(self, request):
-		form = TagForm()
-		return render(request, 'blog/tag_create.html', context={'form': form})
-
-	def post(self, request):
-		bound_form = TagForm(request.POST)
-		
-		if bound_form.is_valid():
-			new_tag = bound_form.save()
-			return redirect(new_tag)
-
-		return render(request, 'blog/tag_create.html', context={'form': bound_form})
-=======
-from django.shortcuts import render
-from .models import Post, Tag
-
-# Create your views here.
-def posts_list(request):
-	title = "Posts list"
-	posts  = Post.objects.all()
-	return render(request, 'blog/index.html', context={'posts': posts, 'title': title})
-
-def post_datayl(request, slug):
-	post = Post.objects.get(slug__iexact=slug)
-	return render(request, 'blog/post_datayl.html', context={'post': post})
-
-def tags_list(request):
-	tags = Tag.objects.all()
-	return render(request, 'blog/tags_list.html', context={'tags': tags})
+class PostCreate(ObjectCreateMixin, View):
+	model_form = PostForm
+	template = 'blog/post_create_form.html'
 
 
-def tag_datayl(request, slug):
-	tag = Tag.objects.get(slug__iexact=slug)
-	return render(request, 'blog/tag_datayl.html', context={'tag': tag})
->>>>>>> 9690f4cfcd0eae61a041a7d20aad8c587c487554
+class TagCreate(ObjectCreateMixin, View):
+	model_form = TagForm
+	template = 'blog/tag_create.html'
